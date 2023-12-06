a55e629c07abc607149cfc422e43621ef6972bf9
Hardheaddinero <shinekwasi2@yahoo.com>
2023-12-06 12:30:59 +0000

    Python Task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    s_len = len(sentence)
-    if s_len == 0:
-        return (0, None)
+    if len(sentence) == 0:
+        return 0, None
     else:
-        present = (s_len, sentence[0])
-        return present
+        return len(sentence), sentence[0]
