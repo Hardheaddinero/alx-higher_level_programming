31cb8503be2882d32ba1f4b517cd6c2f7a4a38f4
Hardheaddinero <shinekwasi2@yahoo.com>
2023-12-06 11:46:20 +0000

    Python Task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    # handle the case where the index (idx) is negative
-    # check if the index is greater than or equal to the number of elements
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
