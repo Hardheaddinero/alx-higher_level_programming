8f96edc89cd008269294134a8b8e5312551fa4ac
Hardheaddinero <shinekwasi2@yahoo.com>
2023-12-06 12:19:42 +0000

    Python Task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    num_row = len(matrix)
-
-    for row in matrix:
-        num_elements = len(row)
-        col = 0  # This will keep track of the column index
-
-        for x in row:
-            if col < num_elements - 1:
-                print("{:d}".format(x), end=" ")
-            else:
-                print("{:d}".format(x), end="")
-            col += 1  # increment column index
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
         print()
